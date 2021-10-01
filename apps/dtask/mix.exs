337d12df7e67bfb0dcdbc5ed8ac4a5893e16379e
Dmitry Kovalev <kdn.kovalev@gmail.com>
2021-09-30 20:27:06 -0500

    [0.1.4] Executor: catch elixir errors and exits on task execution.

--- apps/dtask/mix.exs
+++ apps/dtask/mix.exs
@@ -4,7 +4,7 @@ defmodule DTask.MixProject do
   def project do
     [
       app: :dtask,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.12",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
--- apps/dtask_runner/mix.exs
+++ apps/dtask_runner/mix.exs
@@ -4,7 +4,7 @@ defmodule DTask.App.Runner.MixProject do
   def project do
     [
       app: :dtask_runner,
-      version: "0.1.3",
+      version: "0.1.4",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
