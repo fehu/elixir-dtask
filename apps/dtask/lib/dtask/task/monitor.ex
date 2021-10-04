c7f86cbb0fb7b005f95d07500f90b582dd98fe7e
Dmitry Kovalev <kdn.kovalev@gmail.com>
2021-10-04 16:52:18 -0500

    Task.Monitor: support unconnected state, add `connect` function.

--- apps/dtask/lib/dtask/task/monitor.ex
+++ apps/dtask/lib/dtask/task/monitor.ex
@@ -26,14 +26,21 @@ defmodule DTask.Task.Monitor do
                    state_of: %{Dispatcher.task_id => task_state}
                  }
 
-  @spec start_link(Dispatcher.server) :: GenServer.on_start
+  @spec start_link(Dispatcher.server | nil) :: GenServer.on_start
   def start_link(dispatcher) do
     Logger.debug("DTask.Task.Monitor.start_link(#{inspect(dispatcher)})")
     GenServer.start_link(__MODULE__, dispatcher, name: __MODULE__)
   end
 
-  # # # TODO: Temporary # # #
+  @spec connect(GenServer.server, Dispatcher.server) :: boolean
+  def connect(server \\ __MODULE__, dispatcher) do
+    Logger.debug("DTask.Task.Monitor.connect(#{inspect(server)}, #{inspect(dispatcher)})")
+    if dispatcher,
+       do: GenServer.call(server, {:connect, dispatcher}),
+       else: false
+  end
 
+  @spec get_state(GenServer.server) :: state | nil
   def get_state(server \\ __MODULE__) do
     GenServer.call(server, :get_state)
   end
@@ -42,8 +49,100 @@ defmodule DTask.Task.Monitor do
 
   @impl true
   def init(dispatcher) do
-    tasks = Dispatcher.get_tasks(dispatcher)
+    tasks = if dispatcher, do: fetch_tasks(dispatcher)
+    {:ok, tasks}
+  end
 
+  @impl true
+  def handle_call(:get_state, _, state),
+      do: {:reply, state, state}
+
+  @impl true
+  def handle_call({:connect, dispatcher}, _, state) do
+    tasks = if dispatcher, do: fetch_tasks(dispatcher)
+    {:reply, not is_nil(tasks), tasks}
+  end
+
+  # Execution reports callbacks
+
+  @impl true
+  def handle_cast({:registered, task={task_descriptor, task_id}}, state) when not is_nil(state) do
+    Logger.debug("DTask.Task.Monitor.handle_cast {:registered, #{inspect(task)}}")
+    new_state = state |> put_in([:def_of, task_id], task_descriptor)
+                      |> put_in([:state_of, task_id], :pending)
+    {:noreply, new_state}
+  end
+
+  @impl true
+  def handle_cast({:dispatched, task_id, timestamp, node}, state) when not is_nil(state) do
+    Logger.debug("DTask.Task.Monitor.handle_cast {:dispatched, #{task_id}, #{timestamp}, #{node}}")
+    new_state = put_in(
+      state.state_of[task_id],
+      {:running, %{
+        node: node,
+        progress: :unknown,
+        dispatched: timestamp
+      }}
+    )
+    {:noreply, new_state}
+  end
+
+  @impl true
+  def handle_cast({:progress, task_id, progress}, state) when not is_nil(state) do
+    Logger.debug("DTask.Task.Monitor.handle_cast {:progress, #{task_id}, #{inspect(progress)}}")
+    new_state = update_in(
+      state.state_of[task_id],
+      fn
+        {:running, s} ->
+          {:running, Map.put(s, :progress, progress)}
+        other ->
+          Logger.warning("Progress was reported for task #{task_id}, that is not :running, but #{other}")
+          other
+      end
+    )
+    {:noreply, new_state}
+  end
+
+  @impl true
+  def handle_cast({:finished, task_id, timestamp, outcome}, state) when not is_nil(state) do
+    Logger.debug("DTask.Task.Monitor.handle_cast {:finished, #{task_id}, #{timestamp}, #{inspect(outcome)}}")
+    new_state = update_in(
+      state.state_of[task_id],
+      fn
+        {:running, s} ->
+          {:finished, %{
+            node: s.node,
+            outcome: outcome,
+            dispatched: s.dispatched,
+            finished: timestamp
+          }}
+        other ->
+          Logger.warning("Termination was reported for task #{task_id}, that is not :running, but #{inspect(other)}")
+          other
+      end
+    )
+    {:noreply, new_state}
+  end
+
+  # # # Private functions # # #
+
+  @spec fetch_tasks(Dispatcher.server) :: state | nil
+  defp fetch_tasks(dispatcher) do
+    tasks = try_fetch_tasks(dispatcher)
+    if tasks, do: build_initial_state(tasks)
+  end
+
+  @spec try_fetch_tasks(Dispatcher.server) :: Dispatcher.tasks_state | nil
+  defp try_fetch_tasks(dispatcher) do
+    try do
+      Dispatcher.get_tasks(dispatcher)
+    catch
+      :exit, _ -> nil
+    end
+  end
+
+  @spec build_initial_state(Dispatcher.tasks_state) :: state
+  defp build_initial_state(tasks) do
     def_of_pending =
       for {td, id} <- tasks.pending,
           into: %{} do
@@ -80,76 +179,10 @@ defmodule DTask.Task.Monitor do
         }}}
       end
 
-    {:ok, %{
+    %{
       def_of: def_of_all,
       state_of: state_of_all
-    }}
-  end
-
-  # TODO: Temporary
-
-  @impl true
-  def handle_call(:get_state, _, state), do: {:reply, state, state}
-
-  # Execution reports callbacks
-
-  @impl true
-  def handle_cast({:registered, task={task_descriptor, task_id}}, state) do
-    Logger.debug("DTask.Task.Monitor.handle_cast {:registered, #{inspect(task)}}")
-    new_state = state |> put_in([:def_of, task_id], task_descriptor)
-                      |> put_in([:state_of, task_id], :pending)
-    {:noreply, new_state}
-  end
-
-  @impl true
-  def handle_cast({:dispatched, task_id, timestamp, node}, state) do
-    Logger.debug("DTask.Task.Monitor.handle_cast {:dispatched, #{task_id}, #{timestamp}, #{node}}")
-    new_state = put_in(
-      state.state_of[task_id],
-      {:running, %{
-        node: node,
-        progress: :unknown,
-        dispatched: timestamp
-      }}
-    )
-    {:noreply, new_state}
-  end
-
-  @impl true
-  def handle_cast({:progress, task_id, progress}, state) do
-    Logger.debug("DTask.Task.Monitor.handle_cast {:progress, #{task_id}, #{inspect(progress)}}")
-    new_state = update_in(
-      state.state_of[task_id],
-      fn
-        {:running, s} ->
-          {:running, Map.put(s, :progress, progress)}
-        other ->
-          Logger.warning("Progress was reported for task #{task_id}, that is not :running, but #{other}")
-          other
-      end
-    )
-    {:noreply, new_state}
-  end
-
-  @impl true
-  def handle_cast({:finished, task_id, timestamp, outcome}, state) do
-    Logger.debug("DTask.Task.Monitor.handle_cast {:finished, #{task_id}, #{timestamp}, #{inspect(outcome)}}")
-    new_state = update_in(
-      state.state_of[task_id],
-      fn
-        {:running, s} ->
-          {:finished, %{
-            node: s.node,
-            outcome: outcome,
-            dispatched: s.dispatched,
-            finished: timestamp
-          }}
-        other ->
-          Logger.warning("Termination was reported for task #{task_id}, that is not :running, but #{inspect(other)}")
-          other
-      end
-    )
-    {:noreply, new_state}
+    }
   end
 
   # # # Notifications # # #
