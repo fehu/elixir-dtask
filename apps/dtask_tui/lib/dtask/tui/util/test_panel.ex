96ee324d25e30b8db1b0d79967c1c212201668a1
Dmitry Kovalev <kdn.kovalev@gmail.com>
2021-10-05 14:24:48 -0500

    [DEBUG] Show Data state

--- apps/dtask_tui/lib/dtask/tui/util/test_panel.ex
+++ apps/dtask_tui/lib/dtask/tui/util/test_panel.ex
@@ -3,16 +3,18 @@ defmodule DTask.TUI.Util.TestPanel do
 
 
   @enforce_keys [:title]
-  defstruct [:title, :height]
+  defstruct [:title, :height, :show]
 
   @type height :: non_neg_integer | :fill | nil
 
   @type t :: %__MODULE__{
                title: String.t,
-               height: height
+               height: height,
+               show: term
              }
 
   @type opt :: {:height, height}
+             | {:show, term}
 end
 
 alias DTask.TUI.Util.Render
@@ -24,10 +26,14 @@ defimpl Render, for: TestPanel do
   import DTask.Util.Syntax
 
   @spec render(TestPanel.t, term, [TestPanel.opt]) :: Render.result
-  def render(t, _, opts) do
-    height = Keyword.get(opts <|> [], :height) <|> Map.get(t, :height)
+  def render(t, _, opts0) do
+    opts = opts0 <|> []
+    height = Keyword.get(opts, :height) <|> Map.get(t, :height)
+    show   = Keyword.get(opts, :show)   <|> Map.get(t, :show)
     panel title: t.title, height: height do
-
+      if show,
+         do: label(content: inspect(show, pretty: true, width: 0)),
+         else: []
     end
   end
 end
