e3ed2cba46d5f3bc3d312d3be3f4aaa5e98ed69a
Dmitry Kovalev <kdn.kovalev@gmail.com>
2021-10-07 13:51:51 -0500

    TUI.Data: fallback to Any

--- apps/dtask_tui/lib/dtask/tui/data.ex
+++ apps/dtask_tui/lib/dtask/tui/data.ex
@@ -1,4 +1,5 @@
 defprotocol DTask.TUI.Data do
+  @fallback_to_any true
   @spec save(t) :: [{id :: term, term}]
   def save(data)
 end
@@ -11,4 +12,8 @@ defimpl DTask.TUI.Data, for: DTask.Task.Monitor.State do
   def save(data), do:
     Map.merge(data.def_of, data.state_of, fn _, x, y -> {x, y} end)
     |> Map.to_list
+end
+
+defimpl DTask.TUI.Data, for: Any do
+  def save(_), do: []
 end
