b30a9a00b06ec23657f3d2bd754ffbe85401432b
Dmitry Kovalev <kdn.kovalev@gmail.com>
2021-10-15 16:58:52 -0500

    Revert "Version 0.2.0.1"
    This reverts commit bf5a5349772d5a593d4624ea2be9e437d10aafb4.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule DTaskUmbrella.MixProject do
     [
       # app: :dtask_umbrella,
       apps_path: "apps",
-      version: "0.2.0.1",
+      version: "0.2.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       releases: releases()
