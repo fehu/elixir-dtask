93b39a1acb616cdc7b4fffb4950e82021ef5b0f6
Dmitry Kovalev <kdn.kovalev@gmail.com>
2021-10-17 12:51:05 -0500

    Version 0.2.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule DTaskUmbrella.MixProject do
     [
       # app: :dtask_umbrella,
       apps_path: "apps",
-      version: "0.2.0",
+      version: "0.2.1",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       releases: releases()
